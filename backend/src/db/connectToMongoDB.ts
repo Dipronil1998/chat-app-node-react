fe6dd45bd2b200f01417af2e40d091c1fc8e89ab
Dipronil1998 <dipronildas.net@gmail.com>
2024-07-14 20:20:44 +0530

    backend and mongodb project setup with docker

--- backend/src/db/connectToMongoDB.ts
+++ backend/src/db/connectToMongoDB.ts
@@ -3,8 +3,8 @@ import logger from '../services/logger';
 
 const connectToMongoDB = async (): Promise<void>=>{
     try {
-		await mongoose.connect(process.env.MONGO_DB_URI as string);
-		console.log("Connected to MongoDB");
+		await mongoose.connect(process.env.MONGO_URI as string);
+		console.log("Connected to MongoDB",process.env.MONGO_URI);
 		logger.info('Connected to MongoDB');
 	} catch (error:any) {
 		console.error("Error connecting to MongoDB");
