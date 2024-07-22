713c6c49ad3eef3d0cda3608f8e419b4273d3ae3
dipronil <dipronil.das@techwens.in>
2024-07-22 21:54:45 +0530

    user name show

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,16 +1,17 @@
 import { BiLogOut } from "react-icons/bi";
 import useLogout from "../../hooks/useLogout";
+import { useAuthContext } from "../../context/AuthContext";
 
 const LogoutButton = () => {
 	const { loading, logout } = useLogout();
-
+	const {authUser} = useAuthContext();
 	return (
-		<div className='mt-auto'>
+		<div className='mt-auto flex items-center'>
 			{!loading ? (
 				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
 			) : (
 				<span className='loading loading-spinner'></span>
-			)}
+			)}<p className="ml-2 text-white">{authUser?.response?.fullName}</p>
 		</div>
 	);
 };
